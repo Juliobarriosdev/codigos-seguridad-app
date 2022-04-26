fd957f3b37efe35a73b2580036b8b9cd5d330d65
juliobarrios96 <julio.barrios.9618@gmail.com>
2022-04-26 14:20:04 -0300

    Estados compuestos con useState

--- src/Components/UseState.js
+++ src/Components/UseState.js
@@ -1,50 +1,64 @@
-import { set } from "date-fns";
 import React from "react";
 
 const SECURITY_CODE = 'paradigma';
 
 function UseState({ name }) {
-  const [value, setValue] = React.useState('');
-  const [error, setError] = React.useState(false);
-  const [loading, setLoading] = React.useState(false);
-
-  console.log(value);
+  const [state, setState] = React.useState({
+    value:'',
+    error: false,
+    loading: false
+  });
 
   React.useEffect(() => {
-    
-    if (loading) {
-      // setError(false)
+    if (state.loading) {
       setTimeout(() => {
-        if (value !== SECURITY_CODE) {
-          setError(true); 
+        if (state.value !== SECURITY_CODE) {
+          setState({
+            ...state,
+            error: true,
+          }); 
+          
         } else {
-          setError(false); 
+          setState({
+            ...state,
+            error: false,
+          });
         }
-        setLoading(false);
+        setState((prevState) => ({
+          ...prevState,
+          loading: false
+        }));
       }, 3000)
+      // console.log(state);
+
     }
 
-  }, [loading])
+  }, [state.loading])
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escribe el codigo de seguridad.</p>
-      {(error && !loading) && (
+      {(state.error && !state.loading) && (
         <p>Error: el código es incorrecto</p>
       )}
-      {loading && (
+      {state.loading && (
         <p>Cargando...</p>
       )}
       <input 
         placeholder="Código de seguridad"
-        value={value}
+        value={state.value}
         onChange={(event) => {
-          setValue(event.target.value)
+          setState({
+            ...state,
+            value:event.target.value})
         }}
       />
       <button
-        onClick={() => setLoading(true)}
+        onClick={() => setState({
+          ...state,
+          loading: true
+        })}
       >
         Comprobar
       </button>
